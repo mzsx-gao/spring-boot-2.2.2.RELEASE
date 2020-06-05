49c6c355b86d6a170fd3142c84b075c835ea512d
gaoshudian <18818196720@163.com>
2020-06-05 14:36:53 +0800

    日常维护

--- spring-boot-project/my-springboot/src/main/java/com/gao/springboot/SpringbootDemoApplication.java
+++ spring-boot-project/my-springboot/src/main/java/com/gao/springboot/SpringbootDemoApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-/**
- * Created by gao on 2018/9/13.
- */
 @SpringBootApplication
 @EnableConfigurationProperties({ Person.class, SwaggerProperties.class })
 public class SpringbootDemoApplication {
